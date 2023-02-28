076447a5006c512bab38f01d34b5b8fccbc88881
kotaro <big.i.land.k@gmail.com>
2023-02-28 14:14:36 +0900

    modified

--- JSON_API.py
+++ JSON_API.py
@@ -1,5 +1,5 @@
 import requests
 import urllib
 address = '東京都目黒区駒場３丁目８−１'
-Response =requests.get('https://msearch.gsi.go.jp/address-search/AddressSearch?q='+urllib.parse.quote(address))
-print(Response.json())
+response =requests.get('https://msearch.gsi.go.jp/address-search/AddressSearch?q='+urllib.parse.quote(address))
+print(response.json())
