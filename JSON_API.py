1102d78d3fb1a7604569c1bf8e96379efdf69f34
kotaro <big.i.land.k@gmail.com>
2023-02-28 13:53:10 +0900

    modified JSON_API.py,XML_API.py

--- JSON_API.py
+++ JSON_API.py
@@ -1,6 +1,5 @@
 import requests
 import urllib
-param={'q': '東京都目黒区駒場３丁目８−１'}
-ep="https://msearch.gsi.go.jp/address-search/AddressSearch"
-Response =requests.get(ep,params=param)
+address = '東京都目黒区駒場３丁目８−１'
+Response =requests.get('https://msearch.gsi.go.jp/address-search/AddressSearch?q='+urllib.parse.quote(address))
 print(Response.json())
